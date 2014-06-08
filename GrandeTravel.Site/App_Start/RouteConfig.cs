337e41e0f85f0ed7aed982088c03b661937a5e7b
Rob Grant <robgrantj@yahoo.com.au>
2014-06-08 22:13:56 +1000

    Playing around with route.config

--- GrandeTravel.Site/App_Start/RouteConfig.cs
+++ GrandeTravel.Site/App_Start/RouteConfig.cs
@@ -9,7 +9,7 @@ namespace GrandeTravel.Site
 {
     public class RouteConfig
     {
-      
+
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
